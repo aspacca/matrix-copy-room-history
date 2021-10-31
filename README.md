c5d88983a6810c476f7a3d321998e03e35b2ddb3
Andrea Spacca <andrea.spacca@gmail.com>
2021-10-31 22:16:21 +0100

    working finally

--- README.md
+++ README.md
@@ -25,9 +25,9 @@ $ npm install
 ## Registering as an application service
 The scrip setup a CLI via the `Cli` class, which will dump the registration file to
 `matrix-copy-room-history-registration.yaml`. It will register the user ID `@matrix-copy:domain` and ask
-for exclusive rights (so no one else can create them) to the namespace of every users. It also generates two tokens which will be used for authentication.
+for rights to the namespace of every users and rooms. It also generates two tokens which will be used for authentication.
 
-Now type `DOMAIN=localhost HOMSERVER_URL=http://localhost:9000 node index.js -r -u "http://localhost:9000"` (`HOMSERVER_URL` and the last url param are the same URL that the
+Now type `INVITER=@roomadmin:localhost DOMAIN=localhost HOMSERVER_URL=http://localhost:9000 node index.js -r -u "http://localhost:9000"` (`INVITER` is the Admin user of the Room the history will be copied to, `HOMSERVER_URL` and the last url param are the same URL that the
 homeserver will try to use to communicate with the application service, `DOMAIN` is the DOMAIN of the homserver) and a file
 `matrix-copy-room-history-registration.yaml` will be produced. In your Synapse install, edit
 `homeserver.yaml` to include this file:
@@ -42,7 +42,7 @@ You can save everywhere since you will be able to point to that directory later 
 
 
 ## Defining mapping for rooms
-We need to create a json file to map the id of the Room you want to copy the history from to the Room you want to copy the history to.
+You need to create a json file to map the id of the Room you want to copy the history from to the Room you want to copy the history to.
 
 - `mapped_rooms.json`
 
@@ -56,12 +56,13 @@ The values in the json are the IDs on the new Room to copy to.
 ```
 
 # Run the import
-Run the app service with `DOMAIN=localhost HOMSERVER_URL=http://localhost:9000 ELEMENT_EXPORT_FOLDER=/tmp/ELEMENT-EXPORT-HISTORY node index.js -p 9000` and wait until the last message is print to console and imported on matrix.
+Run the app service with `INVITER=@roomadmin:localhost DOMAIN=localhost HOMSERVER_URL=http://localhost:9000 ELEMENT_EXPORT_FOLDER=/tmp/ELEMENT-EXPORT-HISTORY node index.js -p 9000` and wait until the last message is print to console and copied to the new Room.
 Once it's done you can exit with CTRL+C
 
 
 # Notes
-The messages are sent to the homserver with the `Intent` object obtained from the bridge (https://github.com/matrix-org/matrix-appservice-bridge/blob/develop/README.md#intent).
-This would make sure that user you are importing the message from joined to the room first before sending the message.
-If the user cannot join the room the message is sent to an exception is thrown and the message won't be imported: it's suggested to have all the users on the homeserver already joined the rooms to import for.   
+The messages are sent to the homeserver with the `Intent` object obtained from the bridge (https://github.com/matrix-org/matrix-appservice-bridge/blob/develop/README.md#intent).
+You must create the Room where to copy the history too before running the script. An `INVITER` should also be set (ie: the Admin of the Room where to copy the history): this
+is needed in order to let the Admin user invite the members of the original Room in the new one, in order to prevent troubles with invite only Rooms.
+The script will take care of invitation and joining the new Room by the members of the old one.
 
--- index.js
+++ index.js
@@ -14,46 +14,14 @@ async function asyncForEach(array, callback) {
 
 const ROOMS = require('./mapped_rooms.json');
 
-var htmlEntities = {
-  nbsp: ' ',
-  cent: '¢',
-  pound: '£',
-  yen: '¥',
-  euro: '€',
-  copy: '©',
-  reg: '®',
-  lt: '<',
-  gt: '>',
-  quot: '"',
-  amp: '&',
-  apos: '\''
-};
-
-function unescapeHTML(str) {
-  return str.replace(/\&([^;]+);/g, function (entity, entityCode) {
-    var match;
-
-    if (entityCode in htmlEntities) {
-      return htmlEntities[entityCode];
-      /*eslint no-cond-assign: 0*/
-    } else if (match = entityCode.match(/^#x([\da-fA-F]+)$/)) {
-      return String.fromCharCode(parseInt(match[1], 16));
-      /*eslint no-cond-assign: 0*/
-    } else if (match = entityCode.match(/^#(\d+)$/)) {
-      return String.fromCharCode(~~match[1]);
-    } else {
-      return entity;
-    }
-  });
-}
-
 let bridge;
 
+const INVITER = process.env.INVITER;
 const DOMAIN = process.env.DOMAIN;
 const ELEMENT_EXPORT_FOLDER = process.env.ELEMENT_EXPORT_FOLDER;
 const HOMESERVER_URL = process.env.HOMESERVER_URL;
 
-if (undefined === DOMAIN || undefined === ELEMENT_EXPORT_FOLDER || undefined === HOMESERVER_URL) {
+if (undefined === INVITER || undefined === DOMAIN || undefined === ELEMENT_EXPORT_FOLDER || undefined === HOMESERVER_URL) {
   console.log("Please define all ENV variables (DOMAIN, ELEMENT_EXPORT_FOLDER, HOMESERVER_URL)");
   process.exit(255);
 }
@@ -88,91 +56,105 @@ new Cli({
     bridge.run(port, config).then(async () => {
       let importFolders = fs.readdirSync(ELEMENT_EXPORT_FOLDER).sort();
       await asyncForEach(importFolders, async (jsonFile) => {
+        let hasJoined = {}
+        let isInvited = {}
+
         let mappedEvents = {}
+
+        let ownerIntent = bridge.getIntent(INVITER);
         if (jsonFile.split('.').pop() !== 'json') {
-            return;
+          return;
+        }
+
+        let rawdata = fs.readFileSync(ELEMENT_EXPORT_FOLDER + '/' + jsonFile);
+        let jsonContent = JSON.parse(rawdata);
+        if (!jsonContent.hasOwnProperty('messages')) {
+          return;
+        }
+
+        let messages = jsonContent.messages.sort(function(a,b) {
+          if (a.hasOwnProperty('origin_server_ts') && b.hasOwnProperty('origin_server_ts')) {
+            if (a['origin_server_ts'] > b['origin_server_ts']) {
+              return 1;
+            } else if (a['origin_server_ts'] < b['origin_server_ts']) {
+              return -1;
+            }
           }
 
-          let rawdata = fs.readFileSync(ELEMENT_EXPORT_FOLDER + '/' + jsonFile);
-          let jsonContent = JSON.parse(rawdata);
-          if (!jsonContent.hasOwnProperty('messages')) {
-            return;
-          }
-
-          let messages = jsonContent.messages.sort(function(a,b) {
-            if (a.hasOwnProperty('origin_server_ts') && b.hasOwnProperty('origin_server_ts')) {
-              if (a['origin_server_ts'] > b['origin_server_ts']) {
-                return 1;
-              } else if (a['origin_server_ts'] < b['origin_server_ts']) {
-                return -1;
-              }
-            }
-
-            return 0;
-          });
-
-          await asyncForEach(messages, async (msg) => {
-            if (!msg.hasOwnProperty('type') || msg.type !== "m.room.message") {
-              return;
-            }
-
-            if (!msg.hasOwnProperty('room_id')) {
-              return;
-            }
-
-            let originalRoomId = msg.room_id;
-
-            if (!ROOMS.hasOwnProperty(originalRoomId)) {
-              return;
-            }
-
-            if (!msg.hasOwnProperty('content')) {
-              return;
-            }
-
-            if (!msg.content.hasOwnProperty('body')) {
-              return;
-            }
-
-            let content = msg.content;
-
-            if (!msg.hasOwnProperty('sender')) {
-              return;
-            }
-
-            let userID = msg.sender;
-
-            if (!msg.hasOwnProperty('event_id')) {
-              return;
-            }
-
-            let originalEventId = msg.event_id;
-
-
-            if (content.hasOwnProperty("m.relates.to") &&
-              content["m.relates.to"].hasOwnProperty("m.in_reply_to") &&
-              content["m.relates.to"]["m.in_reply_to"].hasOwnProperty("event_id")) {
-
-              let oldEventID = content["m.relates.to"]["m.in_reply_to"]["event_id"];
-              if (mappedEvents.hasOwnProperty(oldEventID)) {
-                let newEventID = mappedEvents[oldEventID];
-
-                content["m.relates.to"]["m.in_reply_to"]["event_id"] = newEventID;
-              }
-            }
-
-            // content.body = unescapeHTML(content.body);
-
-            let roomID = ROOMS[originalRoomId];
-
-            let intent = bridge.getIntent(userID);
-            await console.log(msg.origin_server_ts, roomID, userID, jsonFile, content);
-            let response = await intent.sendEvent(roomID, "m.room.message", content);
-
-            mappedEvents[originalEventId] = response.event_id;
-
-          });
+          return 0;
         });
+
+        await asyncForEach(messages, async (msg) => {
+          if (!msg.hasOwnProperty('type') || msg.type !== "m.room.message") {
+            return;
+          }
+
+          if (!msg.hasOwnProperty('room_id')) {
+            return;
+          }
+
+          let originalRoomId = msg.room_id;
+
+          if (!ROOMS.hasOwnProperty(originalRoomId)) {
+            return;
+          }
+
+          if (!msg.hasOwnProperty('content')) {
+            return;
+          }
+
+          if (!msg.content.hasOwnProperty('body')) {
+            return;
+          }
+
+          let content = msg.content;
+
+          if (!msg.hasOwnProperty('sender')) {
+            return;
+          }
+
+          let userID = msg.sender;
+
+          if (!msg.hasOwnProperty('event_id')) {
+            return;
+          }
+
+          let originalEventId = msg.event_id;
+
+          if (content.hasOwnProperty("m.relates_to") &&
+            content["m.relates_to"].hasOwnProperty("m.in_reply_to") &&
+            content["m.relates_to"]["m.in_reply_to"].hasOwnProperty("event_id")) {
+
+            let oldEventID = content["m.relates_to"]["m.in_reply_to"]["event_id"];
+            if (mappedEvents.hasOwnProperty(oldEventID)) {
+              let newEventID = mappedEvents[oldEventID];
+
+              content["m.relates_to"]["m.in_reply_to"]["event_id"] = newEventID;
+            }
+          }
+
+          let roomID = ROOMS[originalRoomId];
+
+          if (!isInvited.hasOwnProperty(userID)) {
+            await ownerIntent.invite(roomID, userID);
+            isInvited[userID] = true;
+          }
+
+          let intent = bridge.getIntent(userID);
+
+          if (!hasJoined.hasOwnProperty(userID)) {
+            await intent.join(roomID);
+            hasJoined[userID] = true;
+          }
+
+          await console.log(msg.origin_server_ts, roomID, userID, jsonFile, content);
+
+          let response = await intent.sendEvent(roomID, "m.room.message", content);
+
+          mappedEvents[originalEventId] = response.event_id;
+
+        });
+      });
     }).catch(function(error) {
       console.log(error)
     });
